1ecfaf7ef916528f7346c7af4ab54a0fca5b9c4a
Ramakrishnan83 <rama.r.krishnan@gmail.com>
2021-02-08 19:22:55 -0500

    intial commit

---
+++ src/main/java/com/galvanize/racecar/RacecarApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.racecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacecarApplication.class, args);
+	}
+
+}
